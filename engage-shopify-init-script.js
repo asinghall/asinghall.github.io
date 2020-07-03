43b99f282c01b94523159f33f229a01b2c65aebf
asinghall <abhisinghalibibo@gmail.com>
2020-07-03 17:55:13 +0530

    add engage api

---
+++ engage-shopify-init-script.js
@@ -0,0 +1,13 @@
+ console.log("hello world")
+
+window.sqe = window.sqe || [];
+
+    window.sqe.push(["init", {
+      appId: "APP_1234",
+      iconUrl: "https://picsum.photos/512",
+      vapidKey: "BAgUDkGXAwvAuHiyXIgKa8Wwee730q9Zb2aHVtsRn-k2bCnFJ6C8hkhGCCgbJdUNW_zCblcoprLvyaDSQuASKLA",
+      subscribeTitle: "Subscribe to our updates!",
+      subscribeBody: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+      subscribeButtonText: "Send Updates",
+      subscribeCloseText: "Later"
+    }])
