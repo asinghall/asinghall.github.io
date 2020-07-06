88828d3e0d61f2a9bc64045f36becd6954738b29
asinghall <abhisinghalibibo@gmail.com>
2020-07-06 12:14:19 +0530

    add engage api

--- engage-shopify-init-script.js
+++ engage-shopify-init-script.js
@@ -1,13 +1,10 @@
- console.log("hello world")
-
-window.sqe = window.sqe || [];
-
-    window.sqe.push(["init", {
-      appId: "APP_1234",
-      iconUrl: "https://picsum.photos/512",
-      vapidKey: "BAgUDkGXAwvAuHiyXIgKa8Wwee730q9Zb2aHVtsRn-k2bCnFJ6C8hkhGCCgbJdUNW_zCblcoprLvyaDSQuASKLA",
-      subscribeTitle: "Subscribe to our updates!",
-      subscribeBody: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
-      subscribeButtonText: "Send Updates",
-      subscribeCloseText: "Later"
-    }])
+window.sqe.push(["init", {
+    appId: "APP_1234",
+    iconUrl: "https://picsum.photos/512",
+    vapidKey: "BKfUGMRmN2mT1r2ZBwPRZAtM6BkyB_MRygQa3ENGkEU_2Xd9Wo7N7l8vdKIKbYzLENq4S3U6FYWoT502r5RUdNA",
+    subscribeTitle: "Subscribe to our updates!",
+    subscribeBody: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+    subscribeButtonText: "Send Updates",
+    subscribeCloseText: "Later",
+    serviceWorkerUrl: "/apps/sq-service-worker"
+}]);
